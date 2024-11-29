b8ced1f48bc950427fdaefd8e6bc90b0fbe650fc
Dorkaitz Garcia <dogarcia@birt.eus>
2024-11-29 21:51:51 +0100

    Tarea lista para entregar

--- src/app/interfaces/iarticulo.ts
+++ src/app/interfaces/iarticulo.ts
@@ -9,17 +9,13 @@
  */
 
 export interface ISource {
-    id?: string,
     name: string
 }
 
 export interface IArticulo {
     source: ISource,
-    author?: string,
     title: string,
     description?: string,
     url?: string,
-    urlToImage?: string,
-    publishedAt?: string,
-    content?: string
+    urlToImage?: string
 }
